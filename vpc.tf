768db86230fbd874d7fa8bc42c3f47932fab50b2
dotrashevskyi <d.otrashevskyi@itoutposts.com>
2023-02-01 12:57:01 +0200

    changes for subnets ip range.

--- vpc.tf
+++ vpc.tf
@@ -9,13 +9,13 @@ resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project_id}-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/22" # 1022 addresses
+  ip_cidr_range = "10.10.0.0/21" # 2048 addresses
   secondary_ip_range {
-    ip_cidr_range = "10.10.4.0/23" # 510 addresses -> 440 pods max for 4 nodes
+    ip_cidr_range = "10.10.8.0/21" # 1024 addresses -> 440 pods max for 4 nodes
     range_name    = "pods-addresses-range"
   }
   secondary_ip_range {
-    ip_cidr_range = "10.10.6.0/25" # 126 addresses -> 100 services for example
+    ip_cidr_range = "10.10.16.0/25" # 126 addresses -> 100 services for example
     range_name    = "services-addresses-range"
   }
 }
